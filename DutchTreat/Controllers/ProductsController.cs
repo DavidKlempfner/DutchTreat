a3b2e95e608828da2712e4b934d58283594d511b
davidklempfner1 <david.klempfner@mrisoftware.com>
2021-08-11 21:51:42 +1000

    Added API

---
+++ DutchTreat/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using DutchTreat.Data;
+using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace DutchTreat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductsController : Controller
+    {
+        private readonly IDutchRepository _repository;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(IDutchRepository repository, ILogger<ProductsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            try
+            {
+                return Ok(_repository.GetAllProducts());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
